0d129f42947fdc9333b12dc5994bdd5ab3e34bb0
ErikTromp <e.t.tromp@gmail.com>
2023-12-26 15:37:26 +0100

    First shot at multiprocessing for translating

--- translate_oasst.py
+++ translate_oasst.py
@@ -15,12 +15,14 @@ from trl import SFTTrainer
 import json
 import re
 import sys
+import multiprocessing
 
 # Set up configuration
 target_lang = sys.argv[1]
 checkpoint_location = sys.argv[2]
 checkpoint_n = int(sys.argv[3])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_threads = multiprocessing.cpu_count() if len(sys.argv) < 5 else int(sys.argv[4])
 
 # Load the open assistant dataset
 dataset = load_dataset("OpenAssistant/oasst1")
@@ -101,47 +103,39 @@ def find_largest_checkpoint(checkpoint_location):
     else:
         return 0
 
-# Loop through the actual data and translate
-for fold in dataset:
+# Processes records of a fold inside a thread
+def process_records(records, fold, fold_checkpoint_location, target_lang, cnt_start):
     translated_texts = []
-    cnt = 0
+    for record in records:
+        translated_text = translate_text(record['text'], record['lang'], target_lang) if record['lang'] != target_lang else record['text']
 
-    # Create output folder
+        new_record = {'text': translated_text, 'lang': target_lang if record['lang'] != target_lang else record['lang']}
+        translated_texts.append(new_record)
+    
+    # Write out the processed records to a temporary file
+    temp_file_path = f'{fold_checkpoint_location}/upto_{cnt_start}.json'
+    with open(temp_file_path, 'w', encoding='utf-8') as f:
+        json.dump(translated_texts, f)
+
+    return temp_file_path
+
+# Loop through the actual data and translate in parallel
+for fold in dataset:
+    records = list(dataset[fold])
+    record_chunks = [records[i::num_threads] for i in range(num_threads)]
+
     fold_checkpoint_location = checkpoint_location + '/' + fold
     if not os.path.exists(fold_checkpoint_location):
         os.makedirs(fold_checkpoint_location)
-    
+
     last_checkpoint_n = find_largest_checkpoint(fold_checkpoint_location)
 
-    for record in dataset[fold]:
-        # Check if there is already a checkpoint up to this batch
-        if cnt <= last_checkpoint_n:
-            cnt += 1
-            continue
+    # Create a multiprocessing pool and process each chunk
+    with multiprocessing.Pool(num_threads) as pool:
+        results = pool.starmap(
+            process_records,
+            [(chunk, fold, fold_checkpoint_location, target_lang, last_checkpoint_n + len(chunk) * i) for i, chunk in enumerate(record_chunks)]
+        )
 
-        # Write out checkpoint file
-        if cnt % checkpoint_n == 0 and cnt != 0:
-            print(f"Writing out checkpoint #{cnt}")
-            with open(f'{fold_checkpoint_location}/upto_{cnt}.json', 'w', encoding='utf-8') as f:
-                json.dump(translated_texts, f)
-            translated_texts = []
-
-        translated_text = None
-        new_lang = record['lang']
-        # Translate this record if the language is not the target
-        if record['lang'] != target_lang:
-            translated_text = translate_text(record['text'], record['lang'], target_lang)
-            new_lang = target_lang
-        else:
-            translated_text = record['text']
-
-        # Add full record with translated text
-        new_record = record
-        record['text'] = translated_text
-        record['lang'] = new_lang
-        translated_texts.append(new_record)
-        cnt += 1
-
-    # Write out any remaining records
-    with open(f'{fold_checkpoint_location}/upto_{cnt}.json', 'w', encoding='utf-8') as f:
-        json.dump(translated_texts, f)
+    with open(f'{fold_checkpoint_location}/upto_{len(records)}.json', 'w', encoding='utf-8') as f:
+        json.dump(final_translated_texts, f)
